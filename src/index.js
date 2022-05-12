4d9c4a91dbc07fafe607a5b207cdb09a4b4dbc32
Michael Cumbers <me@michaelcumbers.ca>
2022-05-12 01:13:28 -0700

    Use a slash command instead of watching messages

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS ] });
+const client = new Client({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS ] });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('Do you need a time out?');
+	console.log(`Logged in as ${client.user.tag}!`);
+	client.user.setActivity('Do you need a time out?');
 });
 
-client.on('messageCreate', async message => {
-    try {
-        if (message.system || message.author.bot) return;
-        if (message.content.startsWith('/bigtimeout')) {
-            console.log(`[${message.guild.name}][${message.channel.name}] ${message.author.username} thinks everyone needs a time out...`);
-            await message.guild.members.fetch().then(async fetchedMembers => {
-                await fetchedMembers.forEach(async member => {
-                    try {
-                        await member.timeout(2 * 60 * 1000, 'Everyone needed to shut up for 2 minutes.');
-                        console.log(`[${message.guild.name}][${message.channel.name}] ${member.user.username} has been put in time out.`);
-                    } catch (timeoutError) {
-                        console.log(`[${message.guild.name}][${message.channel.name}] ${member.user.username} could not be put in timeout: ${timeoutError}.`);
-                    }
-                });
-            });
-            var timeoutExpiration = new Date(Date.now() + (2 * 60 * 1000) + (15 * 1000));
-            message.channel.send(`OKAY! EVERYBODY JUST SHUT UP FOR 2 MINUTES! YOU ARE ALL IN A TIME OUT UNTIL ${timeoutExpiration}`);
-        }
-    } catch (e) {
-        console.log(e);
-    }
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === 'bigtimeout') {
+		await interaction.deferReply();
+		const members = await interaction.guild.members.fetch();
+
+		members.forEach(async (member) => {
+			// Don't timeout bots, they did nothing wrong
+			if (member.user.bot) return;
+			// Don't bother trying to timeout people the bot can't moderate or manage--save ourselves the API Errors
+			if (!member.moderatable || !member.manageable) return;
+
+			try {
+				await member.timeout(2 * 60 * 1000, 'Everyone needed to shut up for 2 minutes.');
+				console.log(`[${interaction.guild.name}][${interaction.channel.name}] ${member.user.tag} has been put in time out.`);
+			} catch (timeoutError) {
+				console.log(`[${interaction.guild.name}][${interaction.channel.name}] ${member.user.tag} could not be put in timeout: ${timeoutError}.`);
+			}
+		});
+
+		const timeoutExpiration = new Date(Date.now() + (2 * 60 * 1000) + (15 * 1000));
+		const timeString = `${timeoutExpiration.toLocaleTimeString('en-US')} ${Intl.DateTimeFormat().resolvedOptions().timeZone}`
+		return await interaction.editReply(`OKAY! EVERYBODY JUST SHUT UP FOR 2 MINUTES! YOU ARE ALL IN A TIME OUT UNTIL ${timeString}`);
+	}
 });
 
-client.login(process.env.DISCORD_TOKEN_TIMEOUT);
+client.login(process.env.DISCORD_CLIENT_TOKEN);
